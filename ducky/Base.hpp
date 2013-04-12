10c96da3f2b440e93c46c44aacc1ab49e84685d5
lyase damasked <lyase.d@gmail.com>
2013-04-12 13:35:44 +0200

    reinserted doc & test added some documentation doc  build of doc_pdf  has pb

--- ducky/Base.hpp
+++ ducky/Base.hpp
@@ -1,7 +1,20 @@
 #pragma once
-
+#include <iostream>
 namespace Duck {
-
+/*! A Duck class
+* \details This  is  a base class( an interface) for ducks .
+*this project illustrastes extending a class can have unwanted side effect such as decoy flying just by adding a fly methode to the ducks.
+*\author:  Matthew and Luli: msherborne@gmail.com
+* \version 0.1a
+* \date 1990-2013
+* \pre First initialize the system.
+* \bug should be bug free was check with valgrind in prg testUser_Class_dbo.cpp
+* \warning Improper use can crash your application.
+*
+*/
+/*! \brief this class defines a Base interface for ducks  of the application
+*this class is a minimal duck
+*/
 class Base {
 public:
     void swim();
--- ducky/Decoy.hpp
+++ ducky/Decoy.hpp
@@ -3,7 +3,19 @@
 #include "Base.hpp"
 
 namespace Duck {
-
+/*! A Decoy class
+* \details This class is used to demonstrate side effects if intuitively implemented as a child of duck class it will start flying once duck have a fly method implemented.
+* \author John Doe
+* \version 4.1a
+* \date 1990-2011
+* \pre First initialize the system.
+* \bug should be bug free was check with valgrind in prg testUser_Class_dbo.cpp
+* \warning Improper use can crash your application.
+*
+*/
+/*! \brief this class defines a User object of the application
+*this class now has Posts as children
+*/
 class Decoy : public Base {
 public:
     void virtual display();
