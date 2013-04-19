a32647a2a9614edc09ca14d8c4015dd34af81467
Matthew Sherborne <msherborne@gmail.com>
2013-04-19 18:30:49 +1000

    Made the base class have a virtual destructor

--- weathery/Observer.hpp
+++ weathery/Observer.hpp
@@ -2,6 +2,7 @@ namespace weathery {
 
 struct Observer {
     virtual void update() = 0;
+    virtual ~Observer();
 };
 
 }
--- weathery/Subject.hpp
+++ weathery/Subject.hpp
@@ -6,6 +6,7 @@ struct Subject {
     virtual void registerObserver(Observer*) = 0;
     virtual void removeObserver(Observer*) = 0;
     virtual void notifyObservers(Observer*) = 0;
+    virtual ~Subject();
 };
 
 }
