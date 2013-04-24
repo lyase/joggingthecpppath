7f1bbb9ea7abbff374d75207ae4a389ff15bd814
Matthew Sherborne <msherborne@gmail.com>
2013-04-24 21:40:12 +1000

    Now we have a c++11 observer pattern
    We now have a really nice c++11 observer pattern.
    
    It differs from the Java version in several ways:
    
     1. Instead of an interface, we just declare a simple generic type
        'Observer' which is a callable object, taking the three data params.
     2. The subject (the weather station) only need know about and notify a
        single observer.
     3. The list of observers is maintained by the app. This is quite good
        as it is the app that manages their lifetime too.
     4. There is no need for an observer to know about any interfaces. All
        it need do is provide a call operator that takes three doubles. This
        gives some very loose coupling, but still enables compile time error
        checking.
     5. In Java everything is a shared pointer (reference counted and
        garbage collected). In our app, the app itself manages object
        lifetimes.
     6. If we went the Java way, the list of observers would have been a
        list of pointers to virtual objects, and a notification would be a
        lookup in a virtual method table. However the list of observers here
        is a list of std::function objects that *may* be inlined by the
        compiler when actually called; in the worst case it'll follow 2
        pointers to get the address of a method, which is still faster than
        a vtable lookup.
    
    (evolution of the observer pattern lesson)

--- weather.cpp
+++ weather.cpp
@@ -16,19 +16,34 @@ Released: October 2004
 * =====================================================================================
 */
 #include <iostream>
+#include <vector>
 
 #include "weathery/CurrentConditionsDisplay.hpp"
 #include "weathery/WeatherData.hpp"
 #include "weathery/StatsKeeper.hpp"
 
+/// Takes a callable object, and returns a new std::function that will call that object when that func is called
+template <typename T>
+weathery::Observer asFunc(T& ob) {
+    return [&](double temperature, double humidity, double pressure) {
+        ob(temperature, humidity, pressure);
+    };
+}
+
 int main(int, char**) {
     weathery::LCDDisplay tv;
     weathery::StatsKeeper stats;
-    auto observer = [&](double temperature, double humidity, double pressure) {
-        tv(temperature, humidity, pressure);
-        stats(temperature, humidity, pressure);
+    // A list of observers, each will be called when the weather station emits data
+    std::vector<weathery::Observer> observers {
+        asFunc(tv),
+        asFunc(stats),
     };
-    weathery::WeatherData data(observer);
+    // A quick lambda to call every observer in the list
+    auto notifyAll = [&](double temperature, double humidity, double pressure) {
+        for (auto observer : observers)
+            observer(temperature, humidity, pressure);
+    };
+    weathery::WeatherData data(notifyAll);
     data.setMeasurements(1, 2.2, 3.3);
     data.setMeasurements(10, 20.2, 33.3333333333333333333);
     stats.display();
