df9fe980eb73d514e9537ab73bcb5d26737e937b
lyase damasked <lyase.d@gmail.com>
2013-06-20 18:56:08 +0200

    inserted a pizzastore definition

--- Factory.cpp
+++ Factory.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "factory/Pizza.hpp"
 #include "factory/CheezePizza.hpp"
+#include "factory/pizzashop.hpp"
 int main(int, char**) {
 //    std::unique_ptr<factory::CheezePizza> d1(new factory::CheezePizza());
     factory::Pizza* aPizza;
