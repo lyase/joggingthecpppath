2eafd98a03e67210e69860ba74532d04003461a3
lyase damasked <lyase.d@gmail.com>
2013-12-24 19:23:04 +0100

    using class instead of struc

--- well-managed-collections/MenuItem.hpp
+++ well-managed-collections/MenuItem.hpp
@@ -2,7 +2,13 @@
 
 #include <string>
 
-struct MenuItem {
+class MenuItem {
+public:
+    MenuItem(    std::string _name,std::string _description, bool _vegitarian,    double _price){
+name=_name;
+description= _description;
+  _vegitarian=_vegitarian;
+        price= _price;}
     std::string name;
     std::string description;
     bool vegitarian;
