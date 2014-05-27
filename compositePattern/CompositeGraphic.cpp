d3a956073084607fc11b9e87179a944fafbd82cc
lyase damasked <lyase.d@gmail.com>
2014-05-27 10:27:49 +0200

    a more verbose implementationto point to possible bug c++11 loop may not be same as foreach ??

--- compositePattern/CompositeGraphic.cpp
+++ compositePattern/CompositeGraphic.cpp
@@ -7,12 +7,13 @@
 void CompositeGraphic::print() const
 {
      // for each element in graphicList_, call the print member function
+     std::cout<<"\nloopingthru: "<<graphicList_.size()<<"elements"<<" the old  way\n";
      std::for_each(graphicList_.begin(), graphicList_.end(), std::mem_fun(&Graphic::print));
+     std::cout<<"\nloopingthru: "<<graphicList_.size()<<"elements"<<" the c++11 way\n";
      for(auto it = graphicList_.begin(); it != graphicList_.end() ; ++it) {
           (*it)->print();
      }
 }
-
 void CompositeGraphic::add(Graphic *aGraphic)
 {
      graphicList_.push_back(aGraphic);
