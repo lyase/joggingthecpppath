36314905bccad0621870ebe393a4777f70cd3653
lyase damasked <lyase.d@gmail.com>
2014-05-27 08:41:55 +0200

    the code in cut into hpp &cpp files

---
+++ composite/Ellipse.cpp
@@ -0,0 +1,9 @@
+#include"Ellipse.hpp"
+#include <vector>
+#include <iostream> // std::cout
+#include <memory> // std::auto_ptr
+#include <algorithm> // std::for_each
+#include <functional> // std::mem_fun
+     void Ellipse::print() const {
+          cout << "Ellipse \n";
+     }
--- composite/composite.cpp
+++ composite/composite.cpp
@@ -3,50 +3,22 @@
 #include <memory> // std::auto_ptr
 #include <algorithm> // std::for_each
 #include <functional> // std::mem_fun
+#include "Ellipse.hpp" // std::mem_fun
+#include "CompositeGraphic.hpp" // std::mem_fun
 //build with
 //g++  composite/composite.cpp -o prg
-using namespace std;
-
-class Graphic {
-public:
-     virtual void print() const = 0;
-     virtual ~Graphic() {}
-};
-
-class Ellipse : public Graphic {
-public:
-     void print() const {
-          cout << "Ellipse \n";
-     }
-};
-
-class CompositeGraphic : public Graphic {
-public:
-     void print() const {
-          // for each element in graphicList_, call the print member function
-          for_each(graphicList_.begin(), graphicList_.end(), mem_fun(&Graphic::print));
-     }
-
-     void add(Graphic *aGraphic) {
-          graphicList_.push_back(aGraphic);
-     }
-
-private:
-     vector<Graphic*>  graphicList_;
-};
-
 int main()
 {
      // Initialize four ellipses
-     const auto_ptr<Ellipse> ellipse1(new Ellipse());
-     const auto_ptr<Ellipse> ellipse2(new Ellipse());
-     const auto_ptr<Ellipse> ellipse3(new Ellipse());
-     const auto_ptr<Ellipse> ellipse4(new Ellipse());
+     const std::auto_ptr<Ellipse> ellipse1(new Ellipse());
+     const std::auto_ptr<Ellipse> ellipse2(new Ellipse());
+     const std::auto_ptr<Ellipse> ellipse3(new Ellipse());
+     const std::auto_ptr<Ellipse> ellipse4(new Ellipse());
 
      // Initialize three composite graphics
-     const auto_ptr<CompositeGraphic> graphic(new CompositeGraphic());
-     const auto_ptr<CompositeGraphic> graphic1(new CompositeGraphic());
-     const auto_ptr<CompositeGraphic> graphic2(new CompositeGraphic());
+     const std::auto_ptr<CompositeGraphic> graphic(new CompositeGraphic());
+     const std::auto_ptr<CompositeGraphic> graphic1(new CompositeGraphic());
+     const std::auto_ptr<CompositeGraphic> graphic2(new CompositeGraphic());
 
      // Composes the graphics
      graphic1->add(ellipse1.get());
